b943ccb0302cd146f9ee52e21c3452e90f3eb0c7
Tran Thu <56596865+TranThu2970@users.noreply.github.com>
2021-01-15 00:32:31 +0700

    Update settings.py

--- edoc/settings.py
+++ edoc/settings.py
@@ -91,21 +91,9 @@ DATABASES = {
     }
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
