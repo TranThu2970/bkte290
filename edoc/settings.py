868cd906f9572c83afc1cc252fbf2befc8af43e8
Tran Thu <56596865+TranThu2970@users.noreply.github.com>
2021-01-16 14:19:17 +0700

    debugF

--- edoc/settings.py
+++ edoc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qmdk(q8#r06*63!8++deayj%c(nj12br+w1sy&klx9bk&p^ov4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bkte290.herokuapp.com','127.0.0.1']
 
